d4dd4339fbd21b29320d2ff8a408438130b807c5
Dimentiu <dimentiu4@gmail.com>
2022-11-01 21:18:56 +0200

    1

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,5 @@
-from itertools import product
 from django.db import models
+
 
 class Citi(models.Model):
     cities = models.CharField(max_length=30)
@@ -11,7 +11,7 @@ class Citi(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=30)
     price = models.CharField(max_length=20)
-    
+
     def __str__(self):
         return self.title
 
@@ -19,7 +19,6 @@ class Product(models.Model):
 class Supplier(models.Model):
     citi = models.OneToOneField(Citi, on_delete = models.CASCADE, primary_key = True)
     name_supplier = models.CharField(max_length=20)
-    
 
     def __str__(self):
         return (self.citi, self.name_supplier)
@@ -27,11 +26,11 @@ class Supplier(models.Model):
 
 class Сlient(models.Model):
     name_client = models.CharField(max_length=100)
-    citi = models.ForeignKey(Citi,on_delete = models.CASCADE, primary_key = True)
+    citi = models.ForeignKey(Citi, on_delete = models.CASCADE, primary_key = True)
     product = models.ManyToManyField(Product)
-    
+
     class Meta:
         ordering = ['name_client', 'citi']
-    
+
     def __str__(self):
         return self.name_client
