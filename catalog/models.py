e77714a53e189107081005f9f39a2a223c2c6e13
Dimentiu <dimentiu4@gmail.com>
2022-11-01 21:07:36 +0200

    ДЗ 8. OneToOneField, ForeignKey, ManyToManyField

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,37 @@
+from itertools import product
 from django.db import models
 
-# Create your models here.
+class Citi(models.Model):
+    cities = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.cities
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=30)
+    price = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.title
+
+
+class Supplier(models.Model):
+    citi = models.OneToOneField(Citi, on_delete = models.CASCADE, primary_key = True)
+    name_supplier = models.CharField(max_length=20)
+    
+
+    def __str__(self):
+        return (self.citi, self.name_supplier)
+
+
+class Сlient(models.Model):
+    name_client = models.CharField(max_length=100)
+    citi = models.ForeignKey(Citi,on_delete = models.CASCADE, primary_key = True)
+    product = models.ManyToManyField(Product)
+    
+    class Meta:
+        ordering = ['name_client', 'citi']
+    
+    def __str__(self):
+        return self.name_client
--- first/settings.py
+++ first/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog',
+    'django_extensions',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
